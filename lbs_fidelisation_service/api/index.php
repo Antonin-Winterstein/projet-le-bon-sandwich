d3b425d45eb7bd35a78e740c9d02d3d4667a1014
AntoLhn <liehnantonin@hotmail.fr>
2021-03-15 11:51:16 +0100

    Ajout des routesé

--- lbs_fidelisation_service/api/index.php
+++ lbs_fidelisation_service/api/index.php
@@ -2,6 +2,8 @@
 
 require_once  __DIR__ . '/../src/vendor/autoload.php';
 
+use lbs\fidelisation\controller\FidelisationController;
+
 $api_settings = require_once __DIR__ . '/../src/conf/api_settings.php';
 $api_errors = require_once __DIR__ . '/../src/conf/api_errors.php';
 
@@ -12,6 +14,18 @@ $app = new \Slim\App($api_container);
 
 //*Config et Connexion à la BDD
 $config = parse_ini_file($api_container->get('settings')['dbfile']);
+
+$db = new Illuminate\Database\Capsule\Manager();
+
+/* une instance de connexion */
+$db->addConnection($config); /* configuration avec nos paramètres */
+$db->setAsGlobal();          /* rendre la connexion visible dans tout le projet */
+$db->bootEloquent();         /* établir la connexion */
+
+$app->post('/fidelisation[/]', FidelisationController::class . ':fidelisations');
+
+$app->post('/fidelisation[/]{id}', FidelisationController::class . ':aFidelisations')
+    ->setName('fidelisation');
 
 //* Déclenche le traitement par le framework de la requête courante et la compare dans l'ordre de chacune des routes
 try {
