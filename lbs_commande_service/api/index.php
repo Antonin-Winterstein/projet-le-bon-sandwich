732098e374e5b767032840f3cb700d0fe4de0a3f
AntoLhn <liehnantonin@hotmail.fr>
2021-03-21 19:46:56 +0100

    Ajout header cors

--- lbs_commande_service/api/index.php
+++ lbs_commande_service/api/index.php
@@ -27,8 +27,37 @@ $db->setAsGlobal();          /* rendre la connexion visible dans tout le projet 
 $db->bootEloquent();         /* établir la connexion */
 
 
-//* Les objets de type requête
+// This is the middleware
+// It will add the Access-Control-Allow-Methods header to every request
 
+$app->add(function($request, $response, $next) {
+    $route = $request->getAttribute("route");
+
+    $methods = [];
+
+    if (!empty($route)) {
+        $pattern = $route->getPattern();
+
+        foreach ($this->router->getRoutes() as $route) {
+            if ($pattern === $route->getPattern()) {
+                $methods = array_merge_recursive($methods, $route->getMethods());
+            }
+        }
+        //Methods holds all of the HTTP Verbs that a particular route handles.
+    } else {
+        $methods[] = $request->getMethod();
+    }
+
+    $response = $next($request, $response);
+
+
+    return $response
+        ->withHeader("Access-Control-Allow-Methods", implode(",", $methods))
+        ->withHeader("Access-Control-Allow-Headers", "X-Requested-With, Content-Type, Accept, Origin, Authorization")
+        ->withHeader("Access-Control-Allow-Origin", "https://api.commande.local:19043/");
+});
+
+//* Les objets de type requête
 $app->get('/commandes[/]', CommandController::class . ':commands');
 
 $app->get('/commandes/{id}[/]', CommandController::class . ':aCommand')
